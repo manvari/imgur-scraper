adf266d6a1ddf0754a1f8c82e453a7ef394a8da7
iceTwy <icetwy@icetwy.re>
2014-03-31 22:29:28 +0200

    imgur-scraper is compatible w/ Python 2.6+, too

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages = ['imgurscraper'],
     scripts = ['bin/imgur-scrape'],
     install_requires = ['requests'],
-    version = '0.1',
+    version = '0.1.1',
     description = 'An imgur album scraper',
     long_description = README,
     author = 'iceTwy',
@@ -17,6 +17,9 @@ setup(
     url = 'https://github.com/iceTwy/imgur-scraper',
     keywords = ['imgur', 'album', 'scraper'],
     classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
